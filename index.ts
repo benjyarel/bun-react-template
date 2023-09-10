e1e2ba7b3f2155955827d9759f111b3bab0f4549
Benjamin <b.leray35@gmail.com>
2023-09-10 17:02:27 +0200

    wip -- hot reload config

--- index.ts
+++ index.ts
@@ -1,18 +1,35 @@
+console.log("Starting Bun Server at:", Date.now());
 
-console.log("Starting Bun Server")
 const server = Bun.serve({
   port: 3000,
-  fetch(req) {
+   async fetch(req) {
 
     const url = new URL(req.url);
     console.log("Request to:", url.pathname);
 
     if (url.pathname === '/') {
-      return new Response(Bun.file("./index.html"))
+      return new Response(Bun.file("./public/index.html"))
     }
 
-    if (url.pathname === '/public/index.js') {
-      return  new Response(Bun.file("./public/index.js"))
+      if (url.pathname === '/build/index.js') {
+        console.log("Compilation JS")
+
+      const result = await Bun.build({
+        entrypoints: ['./src/index.tsx'],
+        outdir: './build/',
+      })
+
+      console.log(result.outputs)
+
+      if (!result.success) {
+        console.error("Build failed");
+        for (const message of result.logs) {
+          // Bun will pretty print the message object
+          console.error(message);
+        }
+      }
+
+      return new Response(Bun.file('./build/index.js'))
     }
       return new Response('page not found', {
         status: 404
@@ -20,20 +37,6 @@ const server = Bun.serve({
     }
 });
 
-console.log("Compilation JS")
-
-const result = await Bun.build({
-  entrypoints: ['./src/index.tsx'],
-  outdir: './public/',
-})
-
-if (!result.success) {
-  console.error("Build failed");
-  for (const message of result.logs) {
-    // Bun will pretty print the message object
-    console.error(message);
-  }
-}
 
 
-console.log(`Build sucessful, listening on http://localhost:${server.port} ...`);
+console.log(`Build sucessful, listening on http://localhost:${server.port}...`);
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,5 @@ import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import {Example} from './Example'
 
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Example message="Long live to Bun" />)
+root.render(<Example message="Long live to Bun coucou" />)
