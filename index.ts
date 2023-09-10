ef0300a6e11d63e7a361e64f9b718a297f722c19
Benjamin <b.leray35@gmail.com>
2023-09-10 15:50:11 +0200

    cleanup

--- index.ts
+++ index.ts
@@ -3,21 +3,27 @@ console.log("Starting Bun Server")
 const server = Bun.serve({
   port: 3000,
   fetch(req) {
-    return new Response(Bun.file("./index.html"), {
-      headers: {
-        "Content-Type": "text/html",
-      }
-    })
-  },
-  error: (_e) =>{
-    console.log("whoops, something is wrong with Bun.serve")
-  }
+
+    const url = new URL(req.url);
+    console.log("Request to:", url.pathname);
+
+    if (url.pathname === '/') {
+      return new Response(Bun.file("./index.html"))
+    }
+
+    if (url.pathname === '/public/index.js') {
+      return  new Response(Bun.file("./public/index.js"))
+    }
+      return new Response('page not found', {
+        status: 404
+      })
+    }
 });
 
 console.log("Compilation JS")
 
 const result = await Bun.build({
-  entrypoints: ['./index.tsx'],
+  entrypoints: ['./src/index.tsx'],
   outdir: './public/',
 })
 
