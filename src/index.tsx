dc25940ec6fa65201274dc55af319d7d03ed68dd
Benjamin <b.leray35@gmail.com>
2023-09-12 05:07:28 +0200

    change tsconfig

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { Example } from "./Example";
 
+//@ts-ignore TO FIX: document is not typed by compilerOptions.lib even after DOM lib added
 const root = createRoot(document.getElementById("root"));
 
 root.render(<Example message="Long live to Bun" />);
