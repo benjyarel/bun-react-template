c49f373f9624c4dedc896960de2835a7a8f5f0fc
Benjamin <b.leray35@gmail.com>
2023-09-12 04:03:04 +0200

    cleanup

--- src/example.tsx
+++ src/example.tsx
@@ -1,7 +1,7 @@
 const Example = ({ message }: { message: string}) => {
  return (
   <div>
-    <h1> Welcome to Bun.js with React</h1>
+    <h1> Welcome to Bun.js with React 5</h1>
     <h2>Here is a message from the template creator</h2> 
     <p>{message}</p>
   </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom/client';
+import {createRoot } from 'react-dom/client';
 import {Example} from './Example'
+ 
+const root = createRoot(document.getElementById('root'));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Example message="Long live to Bun coucou" />)
+root.render(<Example message="Long live to Bun coucou 7 " />)
