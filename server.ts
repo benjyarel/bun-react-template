11194bdba1eedf7fb56ecaf342da65ab46a922c0
Benjamin Leray <b.leray35@gmail.com>
2023-09-12 04:28:33 +0200

    Fix: hot reload setup (#1)
    Cleanup some elements, rebuild front js on request

---
+++ server.ts
@@ -0,0 +1,48 @@
+
+
+
+console.log("Compilation JS")
+
+const runBuild = async () => {
+  console.log("compilation")
+  const result = await Bun.build({
+    entrypoints: ['./src/index.tsx'],
+    outdir: './build/',
+  })
+  console.log("compilation done")
+  
+  if (!result.success) {
+    console.error("Build failed");
+    for (const message of result.logs) {
+      console.error(message);
+    }
+  }
+
+}
+
+
+
+
+const server = Bun.serve({
+  port: 3000,
+  async fetch(req) {
+    const url = new URL(req.url);
+    console.log("Request to:", url.pathname);
+    
+    if (url.pathname === '/') {
+      return new Response(Bun.file("public/index.html"))
+    }
+    
+    if (url.pathname === '/build/index.js') {
+        await runBuild();
+        return new Response(Bun.file('build/index.js'))
+    }
+      return new Response('page not found', {
+        status: 404
+      })
+    }
+});
+
+
+
+console.log(`Build sucessful, listening on http://localhost:${server.port}...`);
---
+++ src/Example/index.ts
@@ -0,0 +1,3 @@
+
+
+export {Example, ExampleProps} from './Example';
